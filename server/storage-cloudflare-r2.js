7ef27de136689bdd54e01f4c0e7716a98efb4200
S.M. Ishaq <s.m.ishaq2@gmail.com>
2025-11-14 08:05:24 +0500

    Update storage-cloudflare-r2.js

--- server/storage-cloudflare-r2.js
+++ server/storage-cloudflare-r2.js
@@ -8,7 +8,7 @@ const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
 const R2_BUCKET_NAME = process.env.R2_BUCKET_NAME;
-const R2_ENDPOINT_URL = process.env.R2_ENDPOINT_URL;
+const R2_ENDPOINT_URL = process.env.R2_ENDPOINT_FIXED;
 
 let s3Client = null;
 
